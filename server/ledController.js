caac0e0028d772fcbf6ca80a3ab31c36d8ef3aae
zodiac403 <zodiac403@gmx.de>
2017-05-01 13:36:27 +0200

    LED Controller: fine tune status levels.

--- server/ledController.js
+++ server/ledController.js
@@ -6,24 +6,25 @@ var Blinkt = require('node-blinkt'),
 leds.setup();
 
 function centigrade2Pixel(temperature) {
-    return Math.min(temperature * 6 / 20 + 1, 7);
+    return Math.min(temperature * 6 / 40 + 1, 7);
 }
 
 module.exports = {
     setTemperature: function(temp){
         leds.setAllPixels(0, 0, 0, 0);
 
+        var temperatureLevel =  centigrade2Pixel(temp);
+
         // display status
-        if(temp < 4) {
+        if(temperatureLevel < 3) {
             leds.setPixel(STATUS_PIXEL, 0, 255, 0, BRIGHTNESS);
-        } else if (temp < 6) {
-            leds.setPixel(STATUS_PIXEL, 200, 200, 0, BRIGHTNESS);
+        } else if (temperatureLevel < 5) {
+            leds.setPixel(STATUS_PIXEL, 255, 200, 20, BRIGHTNESS);
         } else {
             leds.setPixel(STATUS_PIXEL, 255, 0, 0, BRIGHTNESS);
         }
 
         // display temperature
-        var temperatureLevel =  centigrade2Pixel(temp);
         for (var i = 0; i < temperatureLevel; i++) {
             leds.setPixel(i+1, 220,10,40, BRIGHTNESS);
         }
